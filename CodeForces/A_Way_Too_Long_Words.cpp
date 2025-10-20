61ed9616edab62207f3d2da916582f7bc9f1ee8e
23pjuvekar <pratham.p.juvekar@gmail.com>
2025-10-20 00:24:52 -0500

    Practice

--- CodeForces/A_Way_Too_Long_Words.cpp
+++ CodeForces/A_Way_Too_Long_Words.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
 
-
 void solve() {
-    int x;
-    cin >> x;
-    if (x > 2 and x % 2 == 0) {
-        cout << "YES";
+    string word;
+    cin >> word;
+    if (word.length() > 10) {
+        cout << word[0] << word.length() - 2 << word[word.length() - 1] << endl;
     } else {
-        cout << "NO";
+        cout << word << endl;
     }
 }
 
@@ -16,9 +15,9 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     int tc = 1;
-    //cin >> tc;
+    cin >> tc;
     for (int t = 1; t <= tc; t++) {
-        // cout << "Case #" << t << ": ";
+        //cout << "Case #" << t << ": ";
         solve();
     }
 }
